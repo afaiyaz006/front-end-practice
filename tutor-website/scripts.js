4ef7e1755def4d0465c192714dbfa98c9bac4a1a
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-10-22 03:39:34 +0600

    updated

--- tutor-website/scripts.js
+++ tutor-website/scripts.js
@@ -1,2 +1,24 @@
-const navbar = document.getElementsByClassName("nav");
-console.log(navbar)
+const burgermenu = document.querySelector('.navbar .mobile-nav-items');
+const toggleButton = document.querySelector('.navbar .burger-button');
+
+
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+  
+    if (window.scrollY > 0) {
+        console.log("YES")
+        
+        navbar.classList.add('navbar-while-scrolling');
+    } else {
+        navbar.classList.remove('navbar-while-scrolling');
+    }
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    toggleButton.addEventListener('click', function () {
+        console.log("Clicked toggle...")
+        console.log(burgermenu)
+        burgermenu.classList.toggle('active');
+    });
+    });
